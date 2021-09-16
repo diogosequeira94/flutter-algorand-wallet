be2545542d00b5dfeb0734a60cbe4e0bd1a8727e
DiogoSequeira <diogo.sequeira@ctw.bmwgroup.com>
2021-09-16 13:23:41 +0100

    animation

--- lib/presentation/pages/splash_screen.dart
+++ lib/presentation/pages/splash_screen.dart
@@ -5,7 +5,18 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animationEntryLogo;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(duration: const Duration(seconds: 4), vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
